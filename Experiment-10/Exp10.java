4e87ab6c8c34418dcf1dc002464b92c6490b9fee
Muskan Maddheshiya <muskanmaddheshiya03@gmail.com>
2026-06-03 00:29:58 +0530

    Experiment-10

--- Experiment-10/Exp10.java
+++ Experiment-10/Exp10.java
@@ -50,12 +50,12 @@ public static void main(String[] args) {
 // Creating objects 
 SavingsAccount sa = new SavingsAccount(101, "Rahul", 10000); 
 CurrentAccount ca = new CurrentAccount(102, "Anita", 20000); 
-System.out.println("----- Savings Account -----"); 
+System.out.println("---- Savings Account ----"); 
 sa.deposit(2000); 
 sa.displayDetails(); 
 sa.calculateInterest(); 
 System.out.println(); 
-System.out.println("----- Current Account -----"); 
+System.out.println("---- Current Account ----"); 
 ca.deposit(3000); 
 ca.displayDetails(); 
 ca.calculateInterest(); 
